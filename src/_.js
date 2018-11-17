889d0249386e9964ef9ee945a692b7719ba8683d
Rudy <38470001+rudypflores@users.noreply.github.com>
2018-11-17 00:19:57 -0600

    Update _.js

--- src/_.js
+++ src/_.js
@@ -4,9 +4,21 @@ const _ = {
     let lowerClampedValue = Math.max(number, lower);
     let clampedValue = Math.min(lowerClampedValue, upper);
     return clampedValue;
+  },
+  
+  inRange(number, start, end) {
+    if(typeof end === 'undefined') {
+      end = start;
+      start = 0;
+    }
+    if(start > end) {
+      let temp = end;
+      end = start;
+      start = temp;
+    }
+    let isInRange = start <= number && number < end;
+    return isInRange;
   }
-  
-  
 };
 _.clamp(3, 1, 4);
 

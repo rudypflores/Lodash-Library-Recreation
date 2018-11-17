0ccd996dc16c5ee14cd5ccb283f14107339edf33
Rudy Flores <38470001+rudypflores@users.noreply.github.com>
2018-11-16 22:06:04 -0600

    refactor rudy

--- src/_.js
+++ src/_.js
@@ -1,4 +1,14 @@
-const _ = {};
+const _ = {
+  //Clamp our values in the lodash object
+  clamp(number, lower, upper) {
+    let lowerClampedValue = Math.max(number, lower);
+    let clampedValue = Math.min(lowerClampedValue, upper);
+    return clampedValue;
+  }
+  
+  
+};
+_.clamp(3, 1, 4);
 
 // Do not write or modify code below this line.
 module.exports = _;
